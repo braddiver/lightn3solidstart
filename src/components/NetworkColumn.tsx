d0c82669a18d3523ee989a38682bc3c9d0d80a70
Brad Diver <brad_diver@comcast.com>
2024-04-29 14:58:00 -0400

    attempt at focus

--- src/components/NetworkColumn.tsx
+++ src/components/NetworkColumn.tsx
@@ -1,14 +1,48 @@
 import { Text, View } from '@lightningjs/solid';
-import { Index, createEffect, createSignal } from 'solid-js';
+import { For, Index, createEffect, createSignal } from 'solid-js';
 import { rgba } from '../utils';
 import { useFocusManager } from '@lightningjs/solid-primitives';
+
+const ColumnStyle = {
+  x: 200,
+  y: 55,
+};
+
+const NetworkItemStyle = {
+  width: 400,
+  height: 70,
+  color: rgba(42, 66, 66, 1),
+  focus: {
+    color: rgba(22, 46, 56),
+  },
+};
+
+const textStyle = {
+  fontSize: 32,
+  mountY: 0.5,
+  x: 20,
+  y: 35,
+};
+
+const textRightStyle = {
+  ...textStyle,
+  alpha: 0.6,
+  fontSize: 32,
+  wordwrap: 30,
+  align: 'center', // does not work
+  x: 340,
+  mountY: 0.5,
+  y: 35,
+};
 
 type Props = {
   channels: [string, number][];
 };
+let scrollCount = 0;
 
 export function NetworkColumn(props: Props) {
   const [channelIndex, setChannelIndex] = createSignal(0);
+  const [focusIndex, setFocusIndex] = createSignal(0);
   const [visibleChannels, setVisibleChannels] = createSignal<Props['channels']>(
     [],
   );
@@ -17,7 +51,7 @@ export function NetworkColumn(props: Props) {
   // Update the visible channels when the channelIndex updates
   createEffect(() => {
     let vis: typeof channels = [];
-    for (let i = channelIndex(), j = 0; i < 5; i++, j++) {
+    for (let i = channelIndex(), j = 0; j < 5; i++, j++) {
       vis[j] = channels[i];
     }
     console.warn({ vis });
@@ -29,49 +63,41 @@ export function NetworkColumn(props: Props) {
     Down: ['ArrowDown', 40],
   });
 
-  const ColumnStyle = {
-    x: 200,
-    y: 55,
-  };
-
-  const NetworkItemStyle = {
-    width: 400,
-    height: 70,
-    color: rgba(42, 66, 66, 1),
-  };
-
-  const textStyle = {
-    fontSize: 32,
-    mountY: 0.5,
-    x: 20,
-    y: 35,
-  };
-
-  const textRightStyle = {
-    ...textStyle,
-    alpha: 0.6,
-    fontSize: 32,
-    wordwrap: 30,
-    align: 'center', // does not work
-    x: 340,
-    mountY: 0.5,
-    y: 35,
-  };
+  function isAutoFocus(index: number) {
+    const result = focusIndex() === index;
+    if (result) {
+      console.warn('focus!', focusIndex());
+    }
+    return result;
+  }
 
   return (
-    <View
-      autofocus
-      style={ColumnStyle}
-      onUp={() => {
-        console.log('Up');
-      }}
-      onDown={() => {
-        console.log('Down');
-      }}
-    >
+    <View style={ColumnStyle}>
       <Index each={visibleChannels()}>
         {(channel, i) => (
-          <View style={NetworkItemStyle} y={78 * i}>
+          <View
+            autofocus={isAutoFocus(i)}
+            style={NetworkItemStyle}
+            y={78 * i}
+            onUp={() => {
+              setFocusIndex((prev: number) => {
+                if (prev >= 0) return 0;
+                const indx = prev - 1;
+                console.warn('up', { focusIndex: indx });
+                return indx;
+              });
+              setChannelIndex((prev) => prev - 1);
+            }}
+            onDown={() => {
+              setFocusIndex((prev: number) => {
+                if (prev >= 4) return 4;
+                const indx = prev + 1;
+                console.warn('down', { focusIndex: indx });
+                return indx;
+              });
+              setChannelIndex((prev) => prev + 1);
+            }}
+          >
             <Text style={textStyle}>{channel()[0]}</Text>
             <Text style={textRightStyle}>{channel()[1]}</Text>
           </View>
